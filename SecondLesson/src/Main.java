b178df1d33e9eea55477f38ee61cf9818353bbd4
Gabriel <avasiloaeigabriel@gmail.com>
2023-02-09 19:07:17 +0200

    First update

--- SecondLesson/src/Main.java
+++ SecondLesson/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("It's a new line!");
+        System.out.println("The second line");
     }
 }
