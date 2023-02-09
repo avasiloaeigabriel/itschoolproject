3320f994a8a99949605815d8eacf58ae34d16816
Gabriel <avasiloaeigabriel@gmail.com>
2023-02-09 19:10:58 +0200

    First update

--- SecondLesson/src/Main.java
+++ SecondLesson/src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("It's a new line!");
         System.out.println("The second line");
+        System.out.println("asdfasdfasdf");
     }
 }
